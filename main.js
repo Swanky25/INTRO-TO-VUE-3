9d15d809f2cdda1e6d4f431f1dd5546ac5cfa503
Swanky25 <107227460+Swanky25@users.noreply.github.com>
2022-07-30 00:00:31 -0400

    l4 change

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
+            image: './assets/images/socks_green.jpg',
             inventory: 100,
             // solution
             onSale: true

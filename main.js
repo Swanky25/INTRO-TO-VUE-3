6964302d78f2bcf3efcb05e7e59a04031f79428b
Swanky25 <107227460+Swanky25@users.noreply.github.com>
2022-07-30 02:05:12 -0400

    l5 last

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/green.jpg',
+            image: './assets/images/socks_green.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             // solution

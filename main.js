5bf0335a0bf36c108ecc395bfc249c6c4affc7f4
Swanky25 <107227460+Swanky25@users.noreply.github.com>
2022-07-30 02:16:07 -0400

    l6 check1

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['45% cotton', '20% wool', '35% polyester'],
-            // solution
-            sizes: ['S', 'M', 'L', 'XL'],
-            // solution
+            details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green' },
-              { id: 2235, color: 'blue' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        // solution
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        // solution
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
